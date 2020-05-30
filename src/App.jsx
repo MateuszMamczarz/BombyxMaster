65bc602961062e6e2c20b4d6bab28d8f1fe1fbf3
sedano <alejandro@sedano.me>
2020-05-30 09:13:28 +0200

    Add basic layout

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Picker from './components/picker'
+
+function App() {
+  return (
+    <section className="main-section">
+      <Picker></Picker>
+
+    </section>
+  );
+}
+
+export default App;
---
+++ src/components/picker.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Picker = ({ options, onPick }) => {
+    return (
+        <div className='picker-container'>
+            {/* {options.map(options)} */}
+        </div>
+    )
+}
+
+export default Picker;
