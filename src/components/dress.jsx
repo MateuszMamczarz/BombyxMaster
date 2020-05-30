5db2fed7eb5e1678c8c913c7939236b86e4c288b
Mateusz Mamczarz <mmamczarz@microstrategy.com>
2020-05-30 13:11:19 +0200

    Add areas

--- src/components/dress.jsx
+++ src/components/dress.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
+
 const Dress = () => {
     return (
         <div className='dress-model'>
+            <div className='collar-area'></div>
+            <div className='arms-area'></div>
+            <div className='hip-area'></div>
+            <div className='width-area'></div>
         </div>
     )
 }
